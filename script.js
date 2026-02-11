7eb2594515f10a2a472245f854c461a474d0b915
Hamsyz <61hamzademirci@gmail.com>
2026-02-11 19:05:03 +0300

    css changes and structural changes in js also  added task buttons but didnt implement their js part

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ const container = document.getElementById("taskcontainer");
 //create a loop to load tasks 
 for (const task of [...tasks].reverse()) {
 
+    // first create a div so we can put our task and button in it 
+    const subcontainer = document.createElement("div");
+    // add a class
+    subcontainer.classList.add("subcontainer");
+    // put it in main container
+    container.prepend(subcontainer);
+
     // create a new element (p) in memory
     const newtaskpara = document.createElement("p");
     // fill the newp with text
@@ -22,7 +29,14 @@ for (const task of [...tasks].reverse()) {
     //adding a class to the p element
     newtaskpara.classList.add("tasks");
     //append the newp to the adress
-    container.appendChild(newtaskpara);
+    subcontainer.appendChild(newtaskpara);
+
+    // create a new button in memory
+    const donebutton = document.createElement("button");
+    // adding  a class to the new button
+    donebutton.classList.add("donebutton");
+    //prepend the new p to the adress
+    subcontainer.prepend(donebutton);
 
 }
 
@@ -42,14 +56,28 @@ form.addEventListener("submit", function(e) {
     // clear the input bar
     taskinput.value = "";
 
+    // first create a div so we can put our task and button in it 
+    const subcontainer = document.createElement("div");
+    // add a class
+    subcontainer.classList.add("subcontainer");
+    // put it in main container
+    container.prepend(subcontainer);
+
     // create a new element (p) in memory
     const newtaskpara = document.createElement("p");
     // fill the newp with text
     newtaskpara.textContent = task;
     // adding a class to the p element
     newtaskpara.classList.add("tasks")
-    // append the newp to the adress
-    container.prepend(newtaskpara);
+    // prepend the newp to the adress
+    subcontainer.prepend(newtaskpara);
+
+    // create a new button in memory
+    const donebutton = document.createElement("button")
+    // adding  a class to the new button
+    donebutton.classList.add("donebutton")
+    //prepend the new p to the adress
+    subcontainer.prepend(donebutton)
 
     // update the the data
     localStorage.setItem("taskdata", JSON.stringify(tasks));
